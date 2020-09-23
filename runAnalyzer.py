f11c6e0fad177afd4c6c6a5702dd0bc319c9d536
Kyungmin Park <jazzykm0110@uos.ac.kr>
2020-09-23 11:18:47 +0900

    run analyzer

--- runAnalyzer.py
+++ runAnalyzer.py
@@ -48,7 +48,7 @@ if configDic['year'] == 2017 and not 'data' in datafile:
         configDic['outname'] = (datafile.split('/')[5]).split('_13TeV')[0] + '_13TeV.root'
     
 #runcode = '$CMSSW_BASE/src/NanoAOD_Analyzer/Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
-runcode = codeDir + 'Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
+runcode = analyzerDir + 'Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
 
 if len(sys.argv) == 3 and '-t' in sys.argv[2]:
     runcode += ' -t'
